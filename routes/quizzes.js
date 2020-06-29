04558dedb27ff84ebe80a38d39de17a063f05b97
Rishav Baral <rishavbaral6@gmail.com>
2020-06-29 20:44:53 +0000

    change up the styling and the layout

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -42,12 +42,28 @@ module.exports = (db) => {
             VALUES($1, $2, $3), ($1, $4, $3), ($1, $5, $3), ($1, $6, $3);`,
             [questionId, req.body.answer_text1, false, req.body.answer_text2, req.body.answer_text3, req.body.answer_text4])
               .then(function(answer1) {
-                res.json(answer1.rows);
+                // res.json(answer1.rows);
               });
+
           });
 
+        console.log('quizid is', quizId);
+        db.query(`INSERT INTO questions (quiz_id, question_text)
+        VALUES($1, $2)
+        RETURNING id;`, [quizId, req.body.question2_text])
+          .then(function(question) {
+            const questionId = question.rows[0].id;
+            console.log('questionid is', questionId);
+
+            db.query(`INSERT INTO answers (question_id, answer_text, true_false)
+            VALUES($1, $2, $3), ($1, $4, $3), ($1, $5, $3), ($1, $6, $3);`,
+            [questionId, req.body.answer2_text1, false, req.body.answer2_text2, req.body.answer2_text3, req.body.answer2_text4])
+              .then(function(answerq2) {
+                console.log('answer q2 is  giving me problems   ', answerq2);
+                res.json(answerq2.rows);
+              });
+          });
       });
   });
-
   return router;
 };
