df2b923f02a50121666b7c3a75075584ea7b1f9c
Rishav Baral <rishavbaral6@gmail.com>
2020-07-02 00:24:41 +0000

    fix the styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,3 @@
-
 const imageRotation = () => {
   const images = ['https://pixabay.com/get/54e1dc424850a514f1dc8460da2932771639dae55a5972_640.png', 'https://pixabay.com/get/54e1d6444c54a814f1dc8460da2932771639dded515976_640.jpg', 'https://pixabay.com/get/52e2d7414d50a414f1dc8460da2932771639dded575672_640.jpg', 'https://pixabay.com/get/52e2d7414e52a514f1dc8460da2932771639dded555170_640.jpg', 'https://pixabay.com/get/55e6d1474e53a814f1dc8460da2932771639dded555374_640.png'];
 
@@ -7,7 +6,6 @@ const imageRotation = () => {
 
 const createQuizElement = quiz => {
 
-const createQuizElement = quiz => {
   const quizElement = `
   <a href="http://localhost:8080/quizzes/${quiz.id}">
     <div class ="quizzes-display">
@@ -66,3 +64,4 @@ $(document).ready(function() {
       });
   });
 });
+
